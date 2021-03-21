c57921ee5186bb7a6369826249930d8f1f6b9ce9
Debjit Chowdhury <41549881+Debjit196@users.noreply.github.com>
2021-03-22 02:34:03 +0530

    updates

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,2 @@
-export const SERVER_URL = 'http://localhost:8080/';
-export const ROLL_NUMBER = '1806100'; // Replace <YOUR_ROLL_NUMBER> with your roll number
+export const SERVER_URL = "http://localhost:8080/";
+export const ROLL_NUMBER = "1828239"; // Replace <YOUR_ROLL_NUMBER> with your roll number
